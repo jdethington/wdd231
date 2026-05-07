58ca5e31e51736852867491715bb0cff60a4bc21
jdethington <jdethington@gmail.com>
2026-05-07 06:36:58 -0700

    getting started

---
+++ scripts/date.js
@@ -0,0 +1,5 @@
+document.getElementById("lastModified").innerHTML = document.lastModified;
+document.getElementById("currentYear").innerHTML = `&copy;${new Date().getFullYear()} `;
+// const today = new Date();
+// const year = document.getElementById("currentYear");
+// year.innerHTML = `&copy; ${today.getFullYear()}`;
