a805b726be485fb03bbd643db075b2b3fa16ec70
jdethington <jdethington@gmail.com>
2026-06-11 16:35:08 -0700

    Removed other Final website

--- final-chamber-base/scripts/buttons.mjs
+++
@@ -1,9 +0,0 @@
-// EVENT LISTENERS TO CHANGE GRID VIEW TO LIST VIEW
-
-gridButton.addEventListener('click', () => {
-    cards.classList.replace('list', 'grid');
-});
-
-listButton.addEventListener('click', () => {
-    cards.classList.replace('grid', 'list');
-})
